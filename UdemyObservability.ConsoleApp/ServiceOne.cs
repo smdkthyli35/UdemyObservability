2c567e53b53708a5a4971db8de84c2f9438443e6
Samed Kütahyalı <smdkthyli@gmail.com>
2024-01-13 21:50:38 +0300

    Events, Status Code and Tags

--- UdemyObservability.ConsoleApp/ServiceOne.cs
+++ UdemyObservability.ConsoleApp/ServiceOne.cs
@@ -1,4 +1,6 @@
-﻿namespace UdemyObservability.ConsoleApp
+﻿using System.Diagnostics;
+
+namespace UdemyObservability.ConsoleApp
 {
     internal class ServiceOne
     {
@@ -7,9 +9,28 @@
         internal async Task<int> MakeRequestToGoogle()
         {
             using var activity = ActivitySourceProvider.Source.StartActivity(kind: System.Diagnostics.ActivityKind.Producer, name: "CustomMakeRequestToGoogle");
-            var result = await httpClient.GetAsync("https://www.google.com");
-            var responseContent = await result.Content.ReadAsStringAsync();
-            return responseContent.Length;
+
+            try
+            {
+                ActivityTagsCollection eventTags = new();
+                activity?.AddEvent(new("Google'a istek başladı!", tags: eventTags));
+                activity?.AddTag("request.schema", "https");
+                activity?.AddTag("request.method", "get");
+
+                var result = await httpClient.GetAsync("https://www.google.com");
+                var responseContent = await result.Content.ReadAsStringAsync();
+
+                activity?.AddTag("response.length", responseContent.Length);
+
+                eventTags.Add("Google Response Lengst", responseContent.Length);
+                activity?.AddEvent(new("Google'a istek tamamlandı!", tags: eventTags));
+                return responseContent.Length;
+            }
+            catch (Exception ex)
+            {
+                activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
+                return -1;
+            }
         }
     }
 }
