15a48abbdf257df967e47d78c5ec7c23474e413b
Samed Kütahyalı <smdkthyli@gmail.com>
2024-01-13 23:11:01 +0300

    Zipkin docker compose added.

--- UdemyObservability.ConsoleApp/Program.cs
+++ UdemyObservability.ConsoleApp/Program.cs
@@ -14,7 +14,10 @@ using var traceProvider = Sdk.CreateTracerProviderBuilder()
                     new KeyValuePair<string, object>("host.machineName", Environment.MachineName),
                     new KeyValuePair<string, object>("host.environment", "dev"),
                 });
-    }).AddConsoleExporter().AddOtlpExporter().Build();
+    }).AddConsoleExporter().AddOtlpExporter().AddZipkinExporter(zipkinOptions =>
+    {
+        zipkinOptions.Endpoint = new Uri("http://localhost:9411/api/v2/spans");
+    }).Build();
 
 var serviceHelper = new ServiceHelper();
 await serviceHelper.Work1();
